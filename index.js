2563e42ebdabbe97ee40420837c5eaf75615356b
ojokne <ojokne@gmail.com>
2023-01-16 20:59:18 +0300

    Format code on index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 if (process.env.NODE_ENV != "production") {
-    require('dotenv').config()
+  require("dotenv").config();
 }
-const app = require('./src/index')
+const app = require("./src/index");
+const { authenticate } = require("./src/config/db");
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, err => {
-    if (err) {
-        console.log("an error occurred");
-    } else {
-        console.log(`Server:: Running on port ${PORT}`);
-    }
-})
+const PORT = process.env.PORT || 5000;
+authenticate();
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("an error occurred");
+  } else {
+    console.log(`Server:: Running on port ${PORT}`);
+  }
+});
