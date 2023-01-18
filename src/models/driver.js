68c5960d4dcc06388321ce5b4fc44d7e2cc2171f
ojokne <ojokne@gmail.com>
2023-01-18 10:31:15 +0300

    Create driver.js

---
+++ src/models/driver.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const Driver = sequelize.define(
+  "Driver",
+  {
+    driverId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    middleName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Driver;
