2e3efa57d95a3592f24c172caf227f442b183907
ojokne <ojokne@gmail.com>
2023-01-18 10:32:04 +0300

    Update client.js

--- src/models/client.js
+++ src/models/client.js
@@ -1,28 +1,46 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
 
-const Client = sequelize.define("Client", {
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
+const Client = sequelize.define(
+  "Client",
+  {
+    clientId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    middleName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  middleName: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING(64),
-    allowNull: false,
-    primaryKey: true,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Client;
