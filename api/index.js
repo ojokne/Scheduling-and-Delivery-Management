646d0cdb3c33f20ac81ee867cc2436174780fb15
ojokne <ojokne@gmail.com>
2023-02-22 10:47:39 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(
     cookie: {
       maxAge: 90000000,
       signed: true,
-      sameSite: "None",
+      // sameSite: "None",
       // secure: true,
     },
     store: new SQLiteStore({ db: "session.db", dir: "./" }),
