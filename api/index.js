3f5cda102a3056926edd155849a7be726957e0f7
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-03-03 00:07:56 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,19 +7,19 @@ const cors = require("cors");
 const routes = require("./routes/index");
 const app = express();
 
-const NETLIFY = ["https://ridelink-client.netlify.app"];
+// const NETLIFY = ["https://ridelink-client.netlify.app"];
 const VERCEL = [
   "https://ridelink-client.vercel.app",
   "https://ridelink-admin.vercel.app",
   "https://ridelink-truck-owner.vercel.app",
   "https://ridelink-driver.vercel.app",
 ];
-const LOCAL = [
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://127.1.1.1:3000",
-  "http://127.2.2.2:3002",
-];
+// const LOCAL = [
+//   "http://localhost:3000",
+//   "http://localhost:3001",
+//   "http://127.1.1.1:3000",
+//   "http://127.2.2.2:3002",
+// ];
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: [...NETLIFY, ...VERCEL, ...LOCAL],
+    origin: [...VERCEL],
   })
 );
 
