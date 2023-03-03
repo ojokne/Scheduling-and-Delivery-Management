0696844f8da0a5d8831d12634a56383473344dcc
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-03-03 08:28:39 +0300

    Refactor

--- api/index.js
+++ api/index.js
@@ -2,51 +2,32 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const initializePassport = require("./config/passport");
-const SQLiteStore = require("connect-sqlite3")(session);
 const cors = require("cors");
 const routes = require("./routes/index");
 const app = express();
 
-// const NETLIFY = ["https://ridelink-client.netlify.app"];
+const NETLIFY = ["https://ridelink-client.netlify.app"];
 const VERCEL = [
   "https://ridelink-client.vercel.app",
   "https://ridelink-admin.vercel.app",
   "https://ridelink-truck-owner.vercel.app",
   "https://ridelink-driver.vercel.app",
 ];
-// const LOCAL = [
-//   "http://localhost:3000",
-//   "http://localhost:3001",
-//   "http://127.1.1.1:3000",
-//   "http://127.2.2.2:3002",
-// ];
+const LOCAL = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://127.1.1.1:3000",
+  "http://127.2.2.2:3002",
+];
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    // credentials: true,
-    origin: [...VERCEL],
+    origin: [...VERCEL, ...LOCAL, ...NETLIFY],
   })
 );
-
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     saveUninitialized: false,
-//     resave: false,
-//     name: "ridelink",
-//     cookie: {
-//       maxAge: 86400000,
-//       signed: true,
-//       sameSite: "none",
-//       secure: true,
-//     },
-//     store: new SQLiteStore({ db: "session.db", dir: "./" }),
-//   })
-// );
-// app.use(passport.authenticate());
 
 initializePassport(passport);
 
